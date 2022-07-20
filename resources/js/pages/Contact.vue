d68a72bda24ab9f1db539dad48d677f17e8ad350
Vijeshwar Datt <vijeshwar.datt@hosttel.com.au>
2022-07-20 15:47:41 +1200

    Contact page changes

--- resources/js/pages/Contact.vue
+++ resources/js/pages/Contact.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <v-container fluid>
-      <h1 style="font-size: 4rem; font-weight: 300;" :class="{ 'text-center' : $vuetify.breakpoint.smAndDown }" class="mb-4">Contact.</h1>
-      <p class="text-center">Coming Soon...</p>
+      <h1 style="font-size: 4rem; font-weight: 300;" :class="{ 'text-center' : $vuetify.breakpoint.smAndDown }" class="mb-4">Contact</h1>
+      <p class="text-center">Coming Soon</p>
     </v-container>
   </div>
 </template>
