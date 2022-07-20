be65da31edde08734402051886397a9260dcc6b3
Vijeshwar Datt <vijeshwar.datt@hosttel.com.au>
2022-07-20 14:27:16 +1200

    Updates to env

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -15,7 +15,7 @@ class Controller extends BaseController
 
   public function index()
   {
-    return url('/assets/images/gallery');
+    // return url('/assets/images/gallery');
     $path = url('/assets/images/gallery');
     $subdirectories = File::directories($path);
     $data = array();
