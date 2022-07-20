3eb8f3edc208eb71de43ffeeb5dabab93ac06825
Vijeshwar Datt <vijeshwar.datt@hosttel.com.au>
2022-07-20 15:51:16 +1200

    Contact page changes

--- resources/js/pages/Bookings.vue
+++ resources/js/pages/Bookings.vue
@@ -2,7 +2,7 @@
   <div>
     <v-container fluid>
       <h1 style="font-size: 4rem; font-weight: 300;" :class="{ 'text-center' : $vuetify.breakpoint.smAndDown }" class="mb-4">Bookings.</h1>
-      <p class="text-center">Coming Soon...</p>
+      <p class="text-center">Coming Soon</p>
     </v-container>
   </div>
 </template>
