0ee42e02df49f81aa8e39b7d16745ca6fee4d388
Vijeshwar Datt <vijeshwar.datt@hosttel.com.au>
2022-07-31 22:17:06 +1200

    Email body testing

--- app/Mail/ContactForm.php
+++ app/Mail/ContactForm.php
@@ -5,7 +5,6 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Queue\SerializesModels;
 
 class ContactForm extends Mailable implements ShouldQueue
 {
