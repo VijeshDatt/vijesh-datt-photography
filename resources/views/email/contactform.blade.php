f6e94fae1f51f1a4b5f6c420c4a02a1ab6c0c680
Vijeshwar Datt <vijeshwar.datt@hosttel.com.au>
2022-07-31 21:51:59 +1200

    Email body testing

--- resources/views/email/contactform.blade.php
+++ resources/views/email/contactform.blade.php
@@ -1,12 +1,5 @@
 @component('mail::message')
-# Greetings
-
-You have a new email/message from {{ $data->name }}.
-
-Subject: {{ $data->subject }}
+# {{ $data->subject }}
 
 {{ $data->message }}
-
-Thanks,<br>
-{{ config('app.name') }}
 @endcomponent
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+<tr>
+  <td class="header">
+    <a href="{{ $url }}">
+      <img src="{{ URL::to('/') }}/assets/logos/black_v2.png" alt="HostTel" height="100px" style="height: 100px; text-align: left;">
+      <!-- {{ $slot }} -->
+    </a>
+  </td>
+</tr>
